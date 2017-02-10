3d4ab855d3c679b2bccb71ec5c31fa02a350ce54
Ina Nizhnikava <ina_nizhnikava@epam.com>
2017-02-10 16:14:42 +0300

    Main Commit

---
+++ src/test/java/Main.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+	@Test
+	public void test() {
+		System.setProperty("webdriver.chrome.driver", "D:\\Program_Files\\selenium server\\chromedriver.exe");
+		
+		System.setProperty("webdriver.chrome.logfile", "target/chrome_output.txt");
+		System.setProperty("webdriver.chrome.args", "--disable-logging");
+		System.setProperty("webdriver.chrome.silentOutput", "true");
+	    
+	    WebDriver driver = new ChromeDriver();
+	    
+	    System.out.println("After get");
+	    
+	    driver.quit();
+	    
+	    System.out.println("After close");
+	}
+	
+}
