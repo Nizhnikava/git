721181d486f053cc2db3f29387670e882a6dcd2a
Nizhnikava <inna.uir@gmail.com>
2017-03-06 19:41:38 +0300

    Update Main.java

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	@Test
 	public void test() {
 		System.out.println("Simple commit");
+		System.out.println("Simple commit");
 		System.setProperty("webdriver.chrome.logfile", "target/chrome_output.txt");
 		System.setProperty("webdriver.chrome.args", "--disable-logging");
 		System.setProperty("webdriver.chrome.silentOutput", "true");
